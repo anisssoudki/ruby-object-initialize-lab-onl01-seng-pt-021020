e5747ca61a466800ac0067ebda1efcf4f0187e6d
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-18 22:59:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   
   attr_accessor :name, :breed
   
-  def initialize(name)
-    @name = name
+  def initialize(dogs_name)
+    @name = dogs_name
   end
   def initialize(breed, default)
     default = "Mutt" 
